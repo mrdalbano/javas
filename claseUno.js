c216e586ed633f4df1de3edeeb5d24ab7418911e
MRDALBANO <mrodriguezdalbano@gmail.com>
2021-06-23 19:56:15 -0300

    first commit

---
+++ claseUno.js
@@ -0,0 +1,12 @@
+
+//const ingreseNombre = prompt("Ingrese su nombre");
+//const ingreseApellido = prompt("Ingrese su apellido");
+//alert("Hola  " + ingreseNombre + " " + ingreseApellido + "!");
+
+let nombreIngresado = prompt ("¿Quien fue el creador de Mafalda?");
+
+if((nombreIngresado == "Quino") || (nombreIngresado == "QUINO")  || (nombreIngresado == "quino") ){
+    alert ("Correcto");
+}else{
+    alert ("Incorrecto.Pruebe de nuevo");
+}
