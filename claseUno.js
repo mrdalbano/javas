996179a2099e72c0588e486f214eec83ec04a38c
MRDALBANO <mrodriguezdalbano@gmail.com>
2021-06-28 18:17:19 -0300

    Proyecto carrito de compras ROBLEX

--- claseUno.js
+++ claseUno.js
@@ -3,17 +3,30 @@
 //const ingreseApellido = prompt("Ingrese su apellido");
 //alert("Hola  " + ingreseNombre + " " + ingreseApellido + "!");
 
-let nombreIngresado = prompt ("¿Cuantos días tiene una semana?");
+//let nombreIngresado = prompt ("¿Cuantos días tiene una semana?");
 
-if((nombreIngresado == "7") || (nombreIngresado == "Siete")  || (nombreIngresado == "SIETE")  || (nombreIngresado == "siete") ){
-   alert ("Correcto");
-}else{
-  alert ("Incorrecto.Pruebe de nuevo");
-}
+//if((nombreIngresado == "7") || (nombreIngresado == "Siete")  || (nombreIngresado == "SIETE")  || (nombreIngresado == "siete") ){
+//   alert ("Correcto");
+//}else{
+//  alert ("Incorrecto.Pruebe de nuevo");
+//}
 
 
-var dias =  ["Lunes", "Martes", "Miercoles", "Jueves", "Viernes", "Sabado", "Domingo"];
+//var dias =  ["Lunes", "Martes", "Miercoles", "Jueves", "Viernes", "Sabado", "Domingo"];
 
-for ( i = 0; i<=dias.length -1; i++ ){
-       console.log( dias[i] + "<br>");
+//for ( i = 0; i<=dias.length -1; i++ ){
+//       console.log( dias[i] + "<br>");
+//}
+
+$(function(){
+  $("input[type=radio]").click(function(){
+    calcular();
+  })
+});
+
+function calcular(){
+  $radios = $("input[type=radio]:checked");
+  $radios.each(function(){
+      alert(1);
+  })
 }
