bd301dc94c6a4dd2368fa176ff2bd048f326ae9b
MRDALBANO <mrodriguezdalbano@gmail.com>
2021-06-28 14:57:30 -0300

    Ciclo

--- claseUno.js
+++ claseUno.js
@@ -3,10 +3,17 @@
 //const ingreseApellido = prompt("Ingrese su apellido");
 //alert("Hola  " + ingreseNombre + " " + ingreseApellido + "!");
 
-let nombreIngresado = prompt ("¿Quien fue el creador de Mafalda?");
+let nombreIngresado = prompt ("¿Cuantos días tiene una semana?");
 
-if((nombreIngresado == "Quino") || (nombreIngresado == "QUINO")  || (nombreIngresado == "quino") ){
-    alert ("Correcto");
+if((nombreIngresado == "7") || (nombreIngresado == "Siete")  || (nombreIngresado == "SIETE")  || (nombreIngresado == "siete") ){
+   alert ("Correcto");
 }else{
-    alert ("Incorrecto.Pruebe de nuevo");
+  alert ("Incorrecto.Pruebe de nuevo");
+}
+
+
+var dias =  ["Lunes", "Martes", "Miercoles", "Jueves", "Viernes", "Sabado", "Domingo"];
+
+for ( i = 0; i<=dias.length -1; i++ ){
+       console.log( dias[i] + "<br>");
 }
